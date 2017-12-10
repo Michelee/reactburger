1a556ec3b13d205f27a4f1c260ad76772d22196a
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-12-10 12:26:28 -0400

    Map ingredients object to array and display jsx

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     //Transform object ingregient to array so all the ingredients can be return as jsx
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i)=> {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             })
-        });
-        console.log(transformedIngredients)
+        })
+        .reduce((arr, el) => {
+            return [...el]
+            //another wat to do the above
+            // return arr.concat(el)
+        }, []);
+        
+        if (transformedIngredients.length === 0){
+            transformedIngredients = (
+                <p>Please Add your Ingredients</p>
+            );
+        }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
