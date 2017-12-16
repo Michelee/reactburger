f27e4f71d74b8122286cc4f0ef143d0ea6c7cc6f
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-12-15 20:35:08 -0400

    Style orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients</p>
-        <p>Price: <strong>USD </strong></p>
-    </div>
-);
+const order = ( props ) => {
+
+    const ingredients = []
+
+    for(let ingredientName in props.ingredients){
+        ingredients.push(
+            {
+                name: ingredientName ,
+                amount:props.ingredients[ingredientName]
+            
+            }
+        );
+    }
+
+    const ingredientsOutput = ingredients.map(ig => {
+        return <span 
+            style={{
+                textTransform: 'capitalize', 
+                display: 'inline-block', 
+                margin: '0 8px', 
+                padding: '5px', 
+                border: '1px solid #ccc'
+            }} 
+            key={ig.name}
+        >{ig.name} ({ig.amount}) </span>
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)} </strong></p>
+        </div>
+    )
+};
 
 export default order;
